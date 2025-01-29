9ef0d1686a2cb8b9e17d6b0958d075611c0d2cd8
NotAShelf <raf@notashelf.dev>
2025-01-29 21:23:36 +0300

    micros/nix-daemon: add certificate authorities

--- micros/modules/services/nix-daemon.nix
+++ micros/modules/services/nix-daemon.nix
@@ -16,12 +16,21 @@ in {
   };
 
   config = mkIf cfg.enable {
-    environment.etc."service/nix/run".source = pkgs.writeScript "start-nix" ''
-      #!${pkgs.runtimeShell}
-      ${cfg.package}/bin/nix-store --load-db < /nix/store/nix-path-registration
+    environment.etc = {
+      "service/nix/run".source = pkgs.writeScript "start-nix" ''
+        #!${pkgs.runtimeShell}
 
-      echo "Starting nix-daemon"
-      ${cfg.package}/bin/nix-daemon
-    '';
+        echo "Starting nix-daemon"
+        ${cfg.package}/bin/nix-daemon
+      '';
+
+      profile.text = lib.mkAfter ''
+        export NIX_PATH="nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos:nixos-config=/etc/nixos/configuration.nix:/nix/var/nix/profiles/per-user/root/channels"
+        export CURL_CA_BUNDLE = "/etc/ssl/certs/ca-certificates.crt";
+      '';
+
+      "ssl/certs/ca-certificates.crt".source = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+      "ssl/certs/ca-bundle.crt".source = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+    };
   };
 }
