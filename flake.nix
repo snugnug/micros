62da6cc97c34fc624c4bf1115e1eb3c1d0849a70
NotAShelf <raf@notashelf.dev>
2025-01-29 03:35:56 +0300

    flake: expose `microsSystem` in top-level lib attribute

--- flake.nix
+++ flake.nix
@@ -12,68 +12,78 @@
     nixpkgs,
     raspi-firmware,
   }: let
-    microsSystem = args:
-      import ./micros/lib/eval-config.nix (
-        {
-          inherit nixpkgs;
-
-          # Allow system to be set modularly in nixpkgs.system.
-          # We set it to null, to remove the "legacy" entrypoint's
-          # non-hermetic default.
-          system = null;
-
-          modules =
-            args.modules
-            ++ [
-              # This module is injected here since it exposes the nixpkgs self-path in as
-              # constrained of contexts as possible to avoid more things depending on it and
-              # introducing unnecessary potential fragility to changes in flakes itself.
-              #
-              # See: failed attempt to make pkgs.path not copy when using flakes:
-              # https://github.com/NixOS/nixpkgs/pull/153594#issuecomment-1023287913
-              ({
-                config,
-                pkgs,
-                lib,
-                ...
-              }: {
-                config.nixpkgs.flake.source = self.outPath;
-              })
-            ];
-        }
-        // builtins.removeAttrs args ["modules"]
-      );
+    inherit (self) lib;
   in {
-    packages.armv7l-linux = let
-      eval = microsSystem {
-        modules = [
-          ./rpi_image.nix
+    packages = {
+      armv7l-linux = let
+        eval = lib.microsSystem {
+          modules = [
+            ./rpi_image.nix
 
-          {
-            system.build.rpi_firmware = raspi-firmware;
-            nixpkgs.hostPlatform = {system = "armv7l-linux";};
-            nixpkgs.buildPlatform = {system = "x86_64-linux";};
-          }
-        ];
+            {
+              system.build.rpi_firmware = raspi-firmware;
+              nixpkgs.hostPlatform = {system = "armv7l-linux";};
+              nixpkgs.buildPlatform = {system = "x86_64-linux";};
+            }
+          ];
+        };
+
+        zynq_eval = lib.microsSystem {
+          modules = [
+            ./zynq_image.nix
+
+            {
+              nixpkgs.hostPlatform = {system = "armv7l-linux";};
+              nixpkgs.buildPlatform = {system = "x86_64-linux";};
+            }
+          ];
+        };
+      in {
+        rpi-image = eval.config.system.build.rpi_image;
+        rpi-image-tar = eval.config.system.build.rpi_image_tar;
+        rpi-runvm = eval.config.system.build.runvm;
+        rpi-toplevel = eval.config.system.build.toplevel;
+
+        zynq-image = zynq_eval.config.system.build.zynq_image;
       };
+    };
 
-      zynq_eval = microsSystem {
-        modules = [
-          ./zynq_image.nix
-
+    # Custom library to provide additional utilities for 3rd party consumption.
+    # Primarily designed to expose `microsSystem` as, e.g., inputs.micros.lib.microsSystem
+    # for when you are building non-supported platforms on your own accord.
+    # TODO: extend nixpkgs.lib here
+    lib = {
+      microsSystem = args:
+        import ./micros/lib/eval-config.nix (
           {
-            nixpkgs.hostPlatform = {system = "armv7l-linux";};
-            nixpkgs.buildPlatform = {system = "x86_64-linux";};
-          }
-        ];
-      };
-    in {
-      rpi-image = eval.config.system.build.rpi_image;
-      rpi-image-tar = eval.config.system.build.rpi_image_tar;
-      rpi-runvm = eval.config.system.build.runvm;
-      rpi-toplevel = eval.config.system.build.toplevel;
+            inherit nixpkgs;
 
-      zynq-image = zynq_eval.config.system.build.zynq_image;
+            # Allow system to be set modularly in nixpkgs.system.
+            # We set it to null, to remove the "legacy" entrypoint's
+            # non-hermetic default.
+            system = null;
+
+            modules =
+              args.modules
+              ++ [
+                # This module is injected here since it exposes the nixpkgs self-path in as
+                # constrained of contexts as possible to avoid more things depending on it and
+                # introducing unnecessary potential fragility to changes in flakes itself.
+                #
+                # See: failed attempt to make pkgs.path not copy when using flakes:
+                # https://github.com/NixOS/nixpkgs/pull/153594#issuecomment-1023287913
+                ({
+                  config,
+                  pkgs,
+                  lib,
+                  ...
+                }: {
+                  config.nixpkgs.flake.source = self.outPath;
+                })
+              ];
+          }
+          // builtins.removeAttrs args ["modules"]
+        );
     };
   };
 }
