2da11c496d3eadaa2075c760397b834b8db982e7
NotAShelf <raf@notashelf.dev>
2025-01-29 20:07:35 +0300

    micros/boot: default to pkgs.linuxPackages

--- micros/modules/boot/kernel.nix
+++ micros/modules/boot/kernel.nix
@@ -5,9 +5,5 @@
   ...
 }: {
   boot.kernelParams = ["systemConfig=${config.system.build.toplevel}"];
-  boot.kernelPackages = lib.mkDefault (
-    if pkgs.system == "armv7l-linux"
-    then pkgs.linuxPackages_rpi1
-    else pkgs.linuxPackages
-  );
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages;
 }
